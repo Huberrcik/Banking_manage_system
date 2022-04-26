fd64df15584c67847f9c1d2a57b68d48bf025d6e
Hubert Brożek <hubert.brozek@wp.pl>
2022-04-26 15:18:20 +0200

    Adding more menu options

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -6,18 +6,35 @@ public class Menu {
     private static int pick;
 
     public static void menu() {
-        System.out.println("Welcome to your online banking system. What would you like to do?");
-        System.out.println("1. Info menu \n2. Transfer \n3. Transfers history \n4. Transfer to your own account \n5. Change your password \n6. Change your PIN \n7. Change your limits");
-        pick = sc.nextInt();
-        switch (pick) {
-            case 1:
-                infoMenu();
+        while(true){
+            System.out.println("Welcome to your online banking system. What would you like to do?");
+            System.out.println("0. Exit \n1. Info menu \n2. Transfer \n3. Transfers history \n4. Transfer to your own account \n5. Change your password \n6. Change your PIN \n7. Change your limits");
+            pick = sc.nextInt();
+            switch (pick) {
+                case 0:
+                    System.exit(0);
+                case 1:
+                    infoMenu();
+                case 2:
+                    System.out.println("Transfer menu to fill");
+                    break;
+                case 3:
+                    System.out.println("Transfer history to fill");
+                    break;
+                case 4:
+                    System.out.println("Transfer to your acc to fill");
+                    break;
+                case 5:
+                    System.out.println("Change password to fill");
+                    break;
+            }
         }
     }
 
     public static void infoMenu() {
         System.out.println("1. Account balance \n2. Account number \n3. User number");
 
+        while(true){
         pick = sc.nextInt();
         switch (pick) {
             case 1:
@@ -29,6 +46,9 @@ public class Menu {
             case 3:
                 System.out.println(UserAccount.getUserID());
                 break;
+            case 0:
+                System.exit(0);
+        }
 
         }
 
